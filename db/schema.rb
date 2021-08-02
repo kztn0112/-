4aa97e8168a1e3edd64c2f321bca2df10b0c9285
kztn0112 <ngwn1995@gmail.com>
2021-08-02 08:08:08 +0000

    [Add]model(user,like)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_074235) do
+ActiveRecord::Schema.define(version: 2021_08_02_080140) do
 
   create_table "answers", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "answer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -42,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_08_02_074235) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
