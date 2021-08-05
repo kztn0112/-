e5098065f302eabbaab0b3703ee9929f325f8565
kztn0112 <ngwn1995@gmail.com>
2021-08-05 13:22:09 +0000

    [追加]いいね機能の非同期通信化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,17 @@
 class LikesController < ApplicationController
   def create
-    answer = Answer.find(params[:answer_id])
-    question = Question.find(params[:question_id])
-    like = current_user.likes.new(answer_id: answer.id)
-    like.save
-    redirect_to question_path(question)
+    @answer = Answer.find(params[:answer_id])
+    @question = Question.find(params[:question_id])
+    @like = current_user.likes.new(answer_id: @answer.id)
+    @like.save
+    # redirect_to question_path(@question)
   end
 
   def destroy
-    answer = Answer.find(params[:answer_id])
-    question = Question.find(params[:question_id])
-    like = current_user.likes.find_by(answer_id: answer.id)
-    like.destroy
-    redirect_to question_path(question)
+    @answer = Answer.find(params[:answer_id])
+    @question = Question.find(params[:question_id])
+    @like = current_user.likes.find_by(answer_id: @answer.id)
+    @like.destroy
+    # redirect_to question_path(@question)
   end
 end
