cadc349159a4a0cfd10e306e49042a50dea842b4
kztn0112 <ngwn1995@gmail.com>
2021-08-13 18:00:45 +0000

    [追加]フラッシュメッセージ、[削除]エラーメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,10 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:info] = '変更が保存されました'
       redirect_to user_path(@user.id)
     else
+      flash.now[:danger] = '名前を入力してください'
       render :edit
     end
   end
