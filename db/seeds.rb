199169ed9efd50f5a8c6afaace16e7f210f0a07a
kztn0112 <ngwn1995@gmail.com>
2021-08-19 11:57:07 +0000

    [追加]初期データ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,74 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+User.create!(
+  [
+    {
+      email: 'user1@gmail.com',
+      password: 'test123',
+      password_confirmation: "test123",
+      name: '山田太郎',
+      introduction: 'パン作り初心者です！よろしくお願いします。',
+      profile_image: File.open('./app/assets/images/user1.png')
+    },
+    {
+      email: 'user2@gmail.com',
+      password: 'test123',
+      password_confirmation: "test123",
+      name: '山田花子',
+      introduction: 'パン作り初心者です！よろしくお願いします。',
+      profile_image: File.open('./app/assets/images/user2.png')
+    }
+  ]
+)
+
+MakingGenre.create!(
+ [
+    {name: '材料'},{name: '生地作り'},{name: '発酵'},{name: '成形'},{name: '焼成'},{name: 'その他'}
+  ]
+)
+
+BreadGenre.create!(
+ [
+    {name: '食パン'},{name: '餡パン'},{name: 'メロンパン'},{name: 'クロワッサン'},{name: 'カレーパン'},{name: 'フランスパン'},{name: 'クリームパン'},{name: 'お惣菜パン'},{name: 'その他'}
+  ]
+)
+
+User.all.each do |user|
+  user.questions.create!(
+    [
+      {
+        title: '食パンについて',
+        content: '食パンについての質問です',
+        user_id: 1,
+        making_genre_id: 1,
+        bread_genre_id: 1,
+        image: File.open('./app/assets/images/bread1.jpg'),
+        is_resolved: false
+      },
+      {
+        title: '餡パンについて',
+        content: '餡パンについての質問です',
+        user_id: 1,
+        making_genre_id: 1,
+        bread_genre_id: 2,
+        image: File.open('./app/assets/images/bread1.jpg'),
+        is_resolved: false
+      },
+      {
+        title: 'メロンパンについ',
+        content: 'メロンパンについての質問です',
+        user_id: 1,
+        making_genre_id: 1,
+        bread_genre_id: 3,
+        image: File.open('./app/assets/images/bread1.jpg'),
+        is_resolved: false
+      }
+    ]
+  )
+end
+
